ea60ec86229e53f27d3460d90006debb9cb65f95
midurbar <midurbar@gmail.com>
2021-01-20 17:00:13 +0100

    modificado app.module.ts

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthModule } from './auth/auth/auth.module';
 import {AppRoutingModule} from './app-routing.module'
@@ -12,6 +13,7 @@ import {AppRoutingModule} from './app-routing.module'
   imports: [
     BrowserModule,
     AppRoutingModule,
+    RouterModule,
     AuthModule
   ],
   providers: [],
