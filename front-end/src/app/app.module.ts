99da7f6fc291d54b2f08f9ca9f02582d2e0ae409
midurbar <midurbar@gmail.com>
2021-01-20 16:46:08 +0100

    Añadido AppRouting

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -2,17 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './auth/components/login/login.component';
-import { RegisterComponent } from './auth/components/register/register.component';
+import { AuthModule } from './auth/auth/auth.module';
+import {AppRoutingModule} from './app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
